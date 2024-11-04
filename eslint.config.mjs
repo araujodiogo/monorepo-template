12ea83708646af2a2ce4017855194bd997ee001e
Diogo Araújo <diogo.guimaraes.araujo@gmail.com>
2024-11-04 02:51:13 +0000

    Monorepo setup: Added eslint v9 support for typescript, react and node; Imported needed dependencies; Creted .yarnrc.yml

---
+++ eslint.config.mjs
@@ -0,0 +1,56 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import rhLint from "eslint-plugin-react-hooks";
+import rLint from "eslint-plugin-react";
+import pluginImport from "eslint-plugin-import";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+
+
+
+export default [
+  {ignores: ["node_modules", "dist"]},
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: {
+    globals: {...globals.browser, ...globals.node},
+    parser: tsParser,
+    parserOptions: {
+      sourceType: "module",
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+  }},
+  {
+    plugins: {
+      react: rLint,
+      "react-hooks": rhLint,
+      "@typescript-eslint": tsPlugin,
+      import: pluginImport,
+      "jsx-a11y": jsxA11y,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...rLint.configs.recommended.rules,
+      ...rhLint.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
+      ...pluginImport.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
+      'no-console': 'warn',
+      'react/no-danger': 'warn',
+      'no-nested-ternary': 'warn',
+      'no-implicit-globals': 'warn',
+      'prefer-const': 'warn',
+      'no-else-return': 'warn',
+      'no-use-before-define': 'warn',
+      'consistent-return': 'warn',
+      'no-return-await': 'warn'
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
