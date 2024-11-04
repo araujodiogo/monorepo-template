9ed9fa7d0e2482f9b255e7be107f816bd3f71974
Diogo Araújo <diogo.guimaraes.araujo@gmail.com>
2024-11-04 03:22:27 +0000

    Added prettier support to project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,36 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import rhLint from "eslint-plugin-react-hooks";
-import rLint from "eslint-plugin-react";
-import pluginImport from "eslint-plugin-import";
-import jsxA11y from "eslint-plugin-jsx-a11y";
-import tsParser from "@typescript-eslint/parser";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
-
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import rhLint from 'eslint-plugin-react-hooks';
+import rLint from 'eslint-plugin-react';
+import pluginImport from 'eslint-plugin-import';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 export default [
-  {ignores: ["node_modules", "dist"]},
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: {
-    globals: {...globals.browser, ...globals.node},
-    parser: tsParser,
-    parserOptions: {
-      sourceType: "module",
-      ecmaFeatures: {
-        jsx: true,
+  { ignores: ['node_modules', 'dist'] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+      parser: tsParser,
+      parserOptions: {
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
       },
     },
-  }},
+  },
   {
     plugins: {
       react: rLint,
-      "react-hooks": rhLint,
-      "@typescript-eslint": tsPlugin,
+      'react-hooks': rhLint,
+      '@typescript-eslint': tsPlugin,
       import: pluginImport,
-      "jsx-a11y": jsxA11y,
+      'jsx-a11y': jsxA11y,
+      prettier: eslintPluginPrettier,
     },
     rules: {
       ...pluginJs.configs.recommended.rules,
@@ -45,11 +47,16 @@ export default [
       'no-else-return': 'warn',
       'no-use-before-define': 'warn',
       'consistent-return': 'warn',
-      'no-return-await': 'warn'
+      'no-return-await': 'warn',
+      'prettier/prettier': 'error',
+      'prefer-template': 'warn',
+      'arrow-parens': 'warn',
+      'object-shorthand': 'warn',
+      'no-var': 'warn',
     },
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
   },
